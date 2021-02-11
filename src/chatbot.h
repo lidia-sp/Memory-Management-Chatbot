4588c9fdddcaa38e6390363a46c51a6c0ab58d45
Lidia Cabanillas <Lidia221189@hotmail.com>
2021-02-11 10:25:31 +0100

    tar file has been extracted

---
+++ src/chatbot.h
@@ -0,0 +1,49 @@
+#ifndef CHATBOT_H_
+#define CHATBOT_H_
+
+#include <wx/bitmap.h>
+#include <string>
+
+class GraphNode; // forward declaration
+class ChatLogic; // forward declaration
+
+class ChatBot
+{
+private:
+    // data handles (owned)
+    wxBitmap *_image; // avatar image
+
+    // data handles (not owned)
+    GraphNode *_currentNode;
+    GraphNode *_rootNode;
+    ChatLogic *_chatLogic;
+
+    // proprietary functions
+    int ComputeLevenshteinDistance(std::string s1, std::string s2);
+
+public:
+    // constructors / destructors
+    ChatBot();                     // constructor WITHOUT memory allocation
+    ChatBot(std::string filename); // constructor WITH memory allocation
+    ~ChatBot();
+
+    //// STUDENT CODE
+    ////
+    ChatBot(const ChatBot &source); //Copy Constructor
+    ChatBot(ChatBot &&source); //Move Constructor
+    ChatBot& operator=(const ChatBot &source); //Copy Assigment Operation
+    ChatBot& operator=(ChatBot &&source); //Move Assigment Operation
+    ////
+    //// EOF STUDENT CODE
+
+    // getters / setters
+    void SetCurrentNode(GraphNode *node);
+    void SetRootNode(GraphNode *rootNode) { _rootNode = rootNode; }
+    void SetChatLogicHandle(ChatLogic *chatLogic) { _chatLogic = chatLogic; }
+    wxBitmap *GetImageHandle() { return _image; }
+
+    // communication
+    void ReceiveMessageFromUser(std::string message);
+};
+
+#endif /* CHATBOT_H_ */
